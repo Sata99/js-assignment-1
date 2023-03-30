5d822add3d2557d1eec099a66de07464da63fc76
Sara Talal <sara.talal99.st@gmail.com>
2023-03-30 23:00:35 +0200

    add a script.js

--- script.js
+++ script.js
@@ -12,47 +12,58 @@
 // getSummary: a function that should summarize your total balances. It should show your total
 // income, your total expenses and also calculate your current balances (income - expenses).
 
-// create a function called menu()
+let account = {
+  firstName : "",
+  income : [],
+  expenses : [],
+  expensesName : [],
+  addIncome : function(amount) {
+  {this.income.push(amount)};
+  },
+  addExpenses : function (amount) {
+    {this.expenses.push(amount);}
+  },
+    listAllExpenses: function(){
+        alert(`Expense you have.\n 
+        ${this.expenses.join(" ")}`);
+  },
+  getSummary: function(){ 
+    alert(`
+    Total income: ${account.income.reduce((sum, income) => sum + income, )}\n
+    
+    Total expenses: ${account.expenses.reduce((sum, expenses) => sum + expenses, )}\n
+    
+    Balance: ${account.income.reduce((sum, income) => sum + income, ) - account.expenses.reduce((sum, expenses) => sum + expenses, 0)}`);
+  },
 
-// the function menu() should only be responsible to show the different choices
-// for the user.
+}
 
-// The menu() function should display the menu with different choices for the user
-// depending on what the user selects from the menu you should call on the correct
-// method or poperty from the account object. You can use a switch or if/else statement for your
-// different menu choices. Please motivate you choice in a comment.
+function menu() {
+  let choice;
+do{
+   choice = parseFloat(
+    prompt("Select a choice 1.)Add income 2.)Add expense 3).List all expenses 4).Get summary"));
+  
+  if(choice===1){
+      let incomeAmount = parseInt(prompt("Enter your income "));
+      account.addIncome(incomeAmount);
+  }
+  if(choice===2){
+    let expenseAmount = 
+    parseInt(prompt("Plase enter your expected amount of expenses : "));
+    prompt("What are your expense?");
+    account.addExpenses(expenseAmount);
+  }
+  if(choice===3){
+    account.listAllExpenses();
+  }
+  if(choice===4){
+  account.getSummary();
+  }
+  }
+  while (choice !== 0);
+}
 
-// CODE TO HELP YOU GET STARTED
+  menu()
 
-// Here are some code snippets to help you get started. You need to place them at the correct
-// place in your code but this is the logic for some parts of your assignment.
-
-// if we want to take an input from the user in a quite nice way we can use the propmt() function
-// the prompt will show a window to the user where we can add a message and it will also display
-// an input field where the user can type something. In order to make this work a bit easier
-// we need to wrap the propmt() in a function called parseFloat(). Why is that? You need to do some
-// googling anf the put your answer in a comment in your code.
-// the syntax you can use looks like this:
-const amount = parseFloat(prompt("How much was your expense?"));
-// the answer that the user will put in the input box will get stored in the variable amount.
-
-// HINT: you will need to use array methods in this assignment and there is one very basic one
-// that is important. It is the method for adding something to an existing array, which one is that?
-
-// In order to show all elements in an array we need to loop through it. In this assignment a
-// forEach loop would be good to use. If you want to display the outcome of each element in the
-// array on separate lines you can add:
-("\n");
-// for example:
-alert("This is a message " + "\n" + "with a new line");
-// to your ouput. This will make the nes output on a new line.
-
-// to make the menu look nice we should not console.log any output. Instead we shoul use either alert()
-// or propmt()
-// we use alert() when we just want to display something to the user, for example a message:
-alert("This is a message in an alert box!");
-// we use the prompt() when we want to ask the user to do something cause the prompt()
-// will display any message that we want and also display an input field where the user
-// can type an input. Like this:
-const age = parseFloat(propmpt("How old are you?"));
-// whatever the user types in the input field will be stored in the variable age.
+  
