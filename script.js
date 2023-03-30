a8d5b430659f9f494af80e56a556435a616a9be3
Sara Talal <sara.talal99.st@gmail.com>
2023-03-30 23:03:19 +0200

    add a new index.html and script.js

--- script.js
+++ script.js
@@ -13,57 +13,57 @@
 // income, your total expenses and also calculate your current balances (income - expenses).
 
 let account = {
-  firstName : "",
-  income : [],
-  expenses : [],
-  expensesName : [],
-  addIncome : function(amount) {
-  {this.income.push(amount)};
-  },
-  addExpenses : function (amount) {
-    {this.expenses.push(amount);}
-  },
-    listAllExpenses: function(){
-        alert(`Expense you have.\n 
-        ${this.expenses.join(" ")}`);
-  },
-  getSummary: function(){ 
-    alert(`
-    Total income: ${account.income.reduce((sum, income) => sum + income, )}\n
-    
-    Total expenses: ${account.expenses.reduce((sum, expenses) => sum + expenses, )}\n
-    
-    Balance: ${account.income.reduce((sum, income) => sum + income, ) - account.expenses.reduce((sum, expenses) => sum + expenses, 0)}`);
-  },
-
-}
-
-function menu() {
-  let choice;
-do{
-   choice = parseFloat(
-    prompt("Select a choice 1.)Add income 2.)Add expense 3).List all expenses 4).Get summary"));
+    firstName : "",
+    income : [],
+    expenses : [],
+    expensesName : [],
+    addIncome : function(amount) {
+    {this.income.push(amount)};
+    },
+    addExpenses : function (amount) {
+      {this.expenses.push(amount);}
+    },
+      listAllExpenses: function(){
+          alert(`Expense you have.\n 
+          ${this.expenses.join(" ")}`);
+    },
+    getSummary: function(){ 
+      alert(`
+      Total income: ${account.income.reduce((sum, income) => sum + income, )}\n
+      
+      Total expenses: ${account.expenses.reduce((sum, expenses) => sum + expenses, )}\n
+      
+      Balance: ${account.income.reduce((sum, income) => sum + income, ) - account.expenses.reduce((sum, expenses) => sum + expenses, 0)}`);
+    },
   
-  if(choice===1){
-      let incomeAmount = parseInt(prompt("Enter your income "));
-      account.addIncome(incomeAmount);
   }
-  if(choice===2){
-    let expenseAmount = 
-    parseInt(prompt("Plase enter your expected amount of expenses : "));
-    prompt("What are your expense?");
-    account.addExpenses(expenseAmount);
-  }
-  if(choice===3){
-    account.listAllExpenses();
-  }
-  if(choice===4){
-  account.getSummary();
-  }
-  }
-  while (choice !== 0);
-}
-
-  menu()
-
   
+  function menu() {
+    let choice;
+  do{
+     choice = parseFloat(
+      prompt("Select a choice 1.)Add income 2.)Add expense 3).List all expenses 4).Get summary"));
+    
+    if(choice===1){
+        let incomeAmount = parseInt(prompt("Enter your income "));
+        account.addIncome(incomeAmount);
+    }
+    if(choice===2){
+      let expenseAmount = 
+      parseInt(prompt("Plase enter your expected amount of expenses : "));
+      prompt("What are your expense?");
+      account.addExpenses(expenseAmount);
+    }
+    if(choice===3){
+      account.listAllExpenses();
+    }
+    if(choice===4){
+    account.getSummary();
+    }
+    }
+    while (choice !== 0);
+  }
+  
+    menu()
+  
+    
